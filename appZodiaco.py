1076ebfa81cf26ae7ce6fbc520a36b765b03d37f
Erick Alexander Rangel Urbina <1719110247@utectulancingo.edu.mx>
2021-02-11 15:42:08 +0000

    pre

---
+++ appZodiaco.py
@@ -0,0 +1,13 @@
+import web
+
+urls = ('.*', 'api_zodiaco.index.Index')
+
+app = web.application(urls, globals())
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+#from os import system
+#system("pkill -9 python")
--- riotgames_api/index.py
+++ riotgames_api/index.py
@@ -4,7 +4,7 @@ import json
 
 #/lol/league/v4/challengerleagues/by-queue/{queue}
 #la api key cambia cada 24 hrs
-api_key = 'RGAPI-66cf651c-8d77-4e8e-ad5b-1e07fd062500'
+api_key = 'RGAPI-1543e424-893f-42d9-8fac-c75e417effd4'
 render = web.template.render("riotgames_api/")
 
 class Index():
@@ -17,22 +17,20 @@ class Index():
         form = web.input()
         tier = form['tier']
         queue = form['queue']
-        #book_name = form["book_name"]
         result = requests.get(
           'https://la1.api.riotgames.com/lol/league/v4/'+str(tier)+'leagues/by-queue/RANKED_'+str(queue)+'?api_key='+str(api_key))
         summoners = result.json()
-        print("funciona")
         items = summoners['entries']
         encoded = json.dumps(items)
         decoded = json.loads(encoded)
         summoner = []
 
         for summ in decoded:
-            summonerName = summ['summonerName']
-            leaguePoints = summ['leaguePoints']
-            wins = summ['wins']
-            hotStreak = summ['hotStreak']
-            TD = {"summonerName": summonerName,'leaguePoints':leaguePoints,'wins':wins,'hotStreak':hotStreak}
-            summoner.append(TD)
+          summonerName = summ['summonerName']
+          leaguePoints = summ['leaguePoints']
+          wins = summ['wins']
+          hotStreak = summ['hotStreak']
+          TD = {"summonerName": summonerName,'leaguePoints':leaguePoints,'wins':wins,'hotStreak':hotStreak}
+          summoner.append(TD)
 
         return render.index(summoner)
